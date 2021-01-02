4a691881b225a7c03fdbbc66307647f20d950132
Ivan soto <iasoto0@gmail.com>
2021-01-02 14:30:16 -0500

    punctuation logic finished

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
 
-
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function(){
-  console.log(document.querySelector('.guess').value);
+
+  const guess  = Number(document.querySelector('.guess').value);
+  
+  if(!guess){
+    document.querySelector('.message').textContent = "📛 No number!";
+       
+  }else if(guess === secretNumber){
+    document.querySelector('.message').textContent = "🎉 Correct Number!";
+    
+  }else if(guess > secretNumber){
+   
+    if(score > 1){
+      document.querySelector('.message').textContent = "📈 Too high!";
+      score--;
+      document.querySelector('.score').textContent = score;
+     
+    }else{
+      document.querySelector('.message').textContent = "💥 You last the game!";
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.check').disabled = true;
+    }
+    
+  }else if(guess < secretNumber){
+
+     if(score > 1){
+      document.querySelector('.message').textContent = "📉 Too low!";
+      score--;
+      document.querySelector('.score').textContent = score;
+     
+    }else{
+      document.querySelector('.message').textContent = "💥 You last the game!";
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('.check').disabled = true;
+    }
+  
+  }
+ 
 })
+
