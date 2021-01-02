eec19871189887e4a6be367894722ca7e8481e11
Ivan soto <iasoto0@gmail.com>
2021-01-02 15:43:25 -0500

    page reset added

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 'use strict';
 
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-console.log(secretNumber);
+let secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 10;
+// console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function(){
 
   const guess  = Number(document.querySelector('.guess').value);
   
+  // When there is no input  
   if(!guess){
     document.querySelector('.message').textContent = "📛 No number!";
-       
+  
+    // When player wins
   }else if(guess === secretNumber){
     document.querySelector('.message').textContent = "🎉 Correct Number!";
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor  = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
     
+   
+  // When gess is too high 
   }else if(guess > secretNumber){
    
     if(score > 1){
@@ -27,7 +34,8 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.score').textContent = 0;
       document.querySelector('.check').disabled = true;
     }
-    
+
+  //  When gess is too low
   }else if(guess < secretNumber){
 
      if(score > 1){
@@ -43,5 +51,21 @@ document.querySelector('.check').addEventListener('click', function(){
   
   }
  
-})
+});
 
+
+document.querySelector('.again').addEventListener('click', function(){
+  score = 10;
+  secretNumber =  Math.trunc(Math.random() * 10) + 1;
+
+  document.querySelector('.message').textContent = "Start guessing...";
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor  = '#222';
+  document.querySelector('.number').style.width = '15rem'; 
+  
+  document.querySelector('.check').disabled = false;
+
+});
